542cb754888cfad56be4c90dcf882cf92334ef27
forati89 <angel3r@hotmail.com>
2019-10-30 17:28:25 +0100

    Updates

---
+++ precache-manifest.f98112028a7b0282e1dbf4f8d80d770d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cb43db39c76e11f3b676ef43161419",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "ec0ffce9683458ffcc81",
+    "url": "/e-shop/static/css/main.027a07be.chunk.css"
+  },
+  {
+    "revision": "534da7d19cee18bf54e9",
+    "url": "/e-shop/static/js/2.4ef63869.chunk.js"
+  },
+  {
+    "revision": "ec0ffce9683458ffcc81",
+    "url": "/e-shop/static/js/main.20c51a11.chunk.js"
+  },
+  {
+    "revision": "0ea210a38830ed9a2f49",
+    "url": "/e-shop/static/js/runtime-main.918bb264.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-shop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-shop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
